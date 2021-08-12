ca2aafefbb5bce47c356d6c44866467c04d9c454
LalitTurbot <lalit@turbot.com>
2021-08-12 14:14:38 +0530

    Add table okta_auth_server

--- okta/table_okta_auth_server.go
+++ okta/table_okta_auth_server.go
@@ -18,7 +18,7 @@ func tableOktaAuthServer() *plugin.Table {
 		Name:        "okta_auth_server",
 		Description: "Represents an Okta user account.",
 		Get: &plugin.GetConfig{
-			Hydrate:           getOktaUserType,
+			Hydrate:           getOktaAuthorizationServer,
 			KeyColumns:        plugin.SingleColumn("id"),
 			ShouldIgnoreError: isNotFoundError([]string{"Not found"}),
 		},
@@ -28,21 +28,22 @@ func tableOktaAuthServer() *plugin.Table {
 
 		Columns: []*plugin.Column{
 			// Top columns
-			// {Name: "name", Type: proto.ColumnType_STRING, Description: "The name for the type."},
-			// {Name: "id", Type: proto.ColumnType_STRING, Description: "Unique key for the User Type."},
-			// {Name: "display_name", Type: proto.ColumnType_STRING, Description: "The display name for the type."},
+			{Name: "name", Type: proto.ColumnType_STRING, Description: "The name of authorization server."},
+			{Name: "id", Type: proto.ColumnType_STRING, Description: "Unique key for the authorization server."},
+			{Name: "status", Type: proto.ColumnType_STRING, Description: "Indicates whether a custom authorization server is ACTIVE or INACTIVE."},
 
-			// // Other columns
-			// {Name: "created", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the User Type was created."},
-			// {Name: "created_by", Type: proto.ColumnType_STRING, Description: "The user ID of the creator of this type."},
-			// {Name: "default", Type: proto.ColumnType_BOOL, Description: "Boolean to indicate if this type is the default."},
-			// {Name: "description", Type: proto.ColumnType_STRING, Description: "A human-readable description of the type."},
-			// {Name: "last_updated", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the User Type was last updated."},
-			// {Name: "last_updated_by", Type: proto.ColumnType_STRING, Description: "The user ID of the last user to edit this type."},
+			// Other columns
+			{Name: "created", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the User Type was created."},
+			{Name: "description", Type: proto.ColumnType_STRING, Description: "The description of a custom authorization server."},
+			{Name: "issuer", Type: proto.ColumnType_STRING, Description: "The complete URL for a custom authorization server. This becomes the iss claim in an access token. issuerMode is visible if you have the custom URL Domain feature enabled."},
+			{Name: "issuer_mode", Type: proto.ColumnType_STRING, Description: "Indicates which value is specified in the issuer of the tokens that a custom authorization server returns: the original Okta org domain URL or a custom domain URL."},
+			{Name: "last_updated", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when authorization server was last updated."},
+
+			{Name: "audiences", Type: proto.ColumnType_JSON, Description: "The recipients that the tokens are intended for. This becomes the aud claim in an access token. Currently, Okta supports only one audience."},
+			{Name: "credentials", Type: proto.ColumnType_JSON, Description: "Keys and settings used to sign tokens."},
 
 			// Steampipe Columns
 			{Name: "title", Type: proto.ColumnType_STRING, Transform: transform.FromField("Name"), Description: titleDescription},
-			{Name: "data", Type: proto.ColumnType_JSON, Transform: transform.FromValue(), Description: titleDescription},
 		},
 	}
 }
@@ -59,7 +60,7 @@ func listOktaAuthorizationServers(ctx context.Context, d *plugin.QueryData, _ *p
 
 	authorizationServers, resp, err := client.AuthorizationServer.ListAuthorizationServers(ctx, &query.Params{})
 	if err != nil {
-		logger.Error("listOktaAuthorizationServers", "list users", err)
+		logger.Error("listOktaAuthorizationServers", "list auth servers", err)
 		return nil, err
 	}
 
@@ -75,37 +76,37 @@ func listOktaAuthorizationServers(ctx context.Context, d *plugin.QueryData, _ *p
 			logger.Error("listOktaAuthorizationServers", "list paging", err)
 			return nil, err
 		}
-		for _, user := range nextAuthorizationServerSet {
-			d.StreamListItem(ctx, user)
+		for _, authorizationServer := range nextAuthorizationServerSet {
+			d.StreamListItem(ctx, authorizationServer)
 		}
 	}
 
 	return nil, err
 }
 
-// //// HYDRATE FUNCTIONS
+//// HYDRATE FUNCTIONS
 
-// func getOktaUserType(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
-// 	logger := plugin.Logger(ctx)
-// 	logger.Debug("getOktaUserType")
+func getOktaAuthorizationServer(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
+	logger := plugin.Logger(ctx)
+	logger.Debug("getOktaAuthorizationServer")
 
-// 	userTypeId := d.KeyColumnQuals["id"].GetStringValue()
+	id := d.KeyColumnQuals["id"].GetStringValue()
 
-// 	if userTypeId == "" {
-// 		return nil, nil
-// 	}
+	if id == "" {
+		return nil, nil
+	}
 
-// 	client, err := Connect(ctx, d)
-// 	if err != nil {
-// 		logger.Error("getOktaUserType", "connect", err)
-// 		return nil, err
-// 	}
+	client, err := Connect(ctx, d)
+	if err != nil {
+		logger.Error("getOktaAuthorizationServer", "connect.error", err)
+		return nil, err
+	}
 
-// 	userType, _, err := client.UserType.GetUserType(ctx, userTypeId)
-// 	if err != nil {
-// 		logger.Error("getOktaUserType", "get user type", err)
-// 		return nil, err
-// 	}
+	authorizationServer, _, err := client.AuthorizationServer.GetAuthorizationServer(ctx, id)
+	if err != nil {
+		logger.Error("getOktaAuthorizationServer", "get_authorization_server.error", err)
+		return nil, err
+	}
 
-// 	return userType, nil
-// }
+	return authorizationServer, nil
+}
