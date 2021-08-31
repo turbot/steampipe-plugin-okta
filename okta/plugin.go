526b6743a5883664f2b1c0fda90421784bf96368
ParthaI <47887552+ParthaI@users.noreply.github.com>
2021-08-31 13:00:41 +0530

    Add table okta_idp_discovery_policy closes #21 (#25)

--- okta/plugin.go
+++ okta/plugin.go
@@ -19,11 +19,12 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			Schema:      ConfigSchema,
 		},
 		TableMap: map[string]*plugin.Table{
-			"okta_application":     tableOktaApplication(),
-			"okta_group":           tableOktaGroup(),
-			"okta_password_policy": tableOktaPasswordPolicy(),
-			"okta_user":            tableOktaUser(),
-			"okta_user_type":       tableOktaUserType(),
+			"okta_application":          tableOktaApplication(),
+			"okta_group":                tableOktaGroup(),
+			"okta_idp_discovery_policy": tableOktaIdpDiscoveryPolicy(),
+			"okta_password_policy":      tableOktaPasswordPolicy(),
+			"okta_user":                 tableOktaUser(),
+			"okta_user_type":            tableOktaUserType(),
 		},
 	}
 
